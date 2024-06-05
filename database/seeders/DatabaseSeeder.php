22dcfeddc377b58a130d41349244c68f8ec4d3bb
simone-danzeri <s.danzeri@icloud.com>
2024-06-05 13:26:27 +0200

    feat: add seeder for types

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Database\Seeders\ProjectsTableSeeder;
+use Database\Seeders\TypesTableSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -24,6 +25,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             ProjectsTableSeeder::class,
+            TypesTableSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/TypesTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Models\Type;
+
+class TypesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $types = ['FrontEnd', 'BackEnd', 'FullStack', 'Design', 'DevOps'];
+        foreach($types as $typeName) {
+            $newType = new Type();
+            $newType->name = $typeName;
+            $newType->slug = Str::slug($newType->name . '-');
+            $newType->save();
+        }
+    }
+}
